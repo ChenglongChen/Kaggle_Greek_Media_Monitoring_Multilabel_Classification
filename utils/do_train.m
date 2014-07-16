eb252996635ab2955ab0191ccc6be8b5d0b68bf3
ChenglongChen <c.chenglong@gmail.com>
2014-07-17 04:16:38 +0800

    refine

--- utils/do_train.m
+++ utils/do_train.m
@@ -11,32 +11,28 @@ elseif nargin == 4
     e = 0.5;
 end
 
+% choose the right type of SVM
+algorithms = { 'l2reg_lr_primal',...
+               'l2reg_l2loss_dual',...
+               'l2reg_l2loss_primal',...
+               'l2reg_l1loss_dual',...
+               'crammer_singer_svc',...
+               'l1reg_l2loss_svc',...
+               'l1reg_lr',...
+               'l2reg_lr_dual',...
+               };
+
+s = find(strcmp(algo, algorithms)) - 1;
+if isempty(s)
+    error('INFO: Unkown option')
+end
+
 % class weights
 w_pos = 1;
 w_neg = sum(y == +1)/sum(y == -1);
 % If you do not want to use class weights, uncomment the following
 w_neg = 1;
-switch algo
-  case 'l2reg_lr_primal'
-    cmd = strcat(['-s 0 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l2reg_l2loss_dual'
-    cmd = strcat(['-s 1 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l2reg_l2loss_primal'
-    cmd = strcat(['-s 2 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l2reg_l1loss_dual'
-    cmd = strcat(['-s 3 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'crammer_singer'
-    cmd = strcat(['-s 4 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l1reg_l2loss_loss'
-    cmd = strcat(['-s 5 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l1reg_lr'
-    cmd = strcat(['-s 6 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  case 'l2reg_lr_dual'
-    cmd = strcat(['-s 7 -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
-  otherwise
-    disp('INFO: Unkown option')
-end
-
+cmd = strcat(['-s ', num2str(s), ' -c ', num2str(C), ' -w1 ', num2str(w_pos,2), ' -w-1 ', num2str(w_neg,2), ' -B -1 -e ', num2str(e), ' -q']);
 model = train(y, x, cmd);
 
 w = model.w(:);
